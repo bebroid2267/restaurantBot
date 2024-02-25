a83d958e886e3c308256654a0f1c5d719085450d
bebroid2267 <148254843+bebroid2267@users.noreply.github.com>
2024-02-25 23:18:04 +0500

    Добавьте файлы проекта.

---
+++ DataBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace restaurantBot
+{
+    public static class DataBase
+    {
+        private static readonly string connectionString = @"Data Source = C:\Users\кирилл\Desktop\restaurant.db";
+
+        public async static Task<bool> IfExistsUser(string userId)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"SELECT count(*) FROM users WHERE user_id LIKE '{userId}'";
+
+                object result = await command.ExecuteScalarAsync();
+                long count = Convert.ToInt64(result);
+
+                if (count > 0)
+                {
+                    await connection.CloseAsync();
+                    return true;
+                }
+                else
+                {
+                    await connection.CloseAsync();
+                    return false;
+                }
+
+            }
+
+        }
+
+        public async static Task<bool> AddUser(string userId, string regDate, string userName)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+
+                if (await IfExistsUser(userId) == false)
+                {
+                    command.CommandText = $"INSERT INTO users (id, user_id, reg_date, user_name) values (@user_id, @reg_date, @user_name)";
+
+                    command.Parameters.AddWithValue("@user_id", userId);
+                    command.Parameters.AddWithValue("@reg_date", regDate);
+                    command.Parameters.AddWithValue("@user_name", userName);
+                    
+                    await command.ExecuteNonQueryAsync();
+                    await connection.CloseAsync();
+                    return true;
+                }
+
+                await connection.CloseAsync();
+                return false;
+                
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+namespace restaurantBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new TelegramBotClient("6717902573:AAFllwaelabWcpQyJI6_BjO8PUOQ1aNWhT4");
+
+            bot.StartReceiving(Update,Error);
+        }
+
+        private static async Task Update(ITelegramBotClient bot, Update update, CancellationToken cts)
+        {
+
+
+        }
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Ошибка телеграм АПИ:\n{apiRequestException.ErrorCode}\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+    }
+}
