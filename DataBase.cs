400bcc2e3f5771308ed6c8dc13effd9f85255e60
Les go baby <porkatube@gmail.com>
2024-02-27 22:42:24 +0500

    в процессе (note) сейчас работаю над выводом доступных столиков по данным заявки на бронь

--- DataBase.cs
+++ DataBase.cs
@@ -9,7 +9,7 @@ namespace restaurantBot
 {
     public static class DataBase
     {
-        private static readonly string connectionString = @"Data Source = C:\Users\кирилл\Desktop\restaurant.db";
+        private static readonly string connectionString = @"Data Source = C:\Users\porka\OneDrive\Рабочий стол\restaurant.db";
 
         public async static Task<bool> IfExistsUser(string userId)
         {
@@ -50,11 +50,12 @@ namespace restaurantBot
 
                 if (await IfExistsUser(userId) == false)
                 {
-                    command.CommandText = $"INSERT INTO users (id, user_id, reg_date, user_name) values (@user_id, @reg_date, @user_name)";
+                    command.CommandText = $"INSERT INTO users (user_id, reg_date, user_name) values (@user_id, @reg_date, @user_name)";
 
                     command.Parameters.AddWithValue("@user_id", userId);
                     command.Parameters.AddWithValue("@reg_date", regDate);
                     command.Parameters.AddWithValue("@user_name", userName);
+
                     
                     await command.ExecuteNonQueryAsync();
                     await connection.CloseAsync();
@@ -67,5 +68,125 @@ namespace restaurantBot
             }
 
         }
+        private async static Task<bool> IfStateEsixsts(string userId)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"SELECT COUNT(*) FROM state_reservetion WHERE user_id LIKE '{userId}'";
+
+                object result = await command.ExecuteScalarAsync();
+                long count = Convert.ToInt64(result);
+
+                if (count > 0)
+                {
+                    await connection.CloseAsync();
+                    return true;
+                }
+                else
+                {
+                    await connection.CloseAsync();
+                    return false;
+                }
+
+            }
+        }
+
+        public async static Task AddCountPeopleState(string userId,string countPeople)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+                if (await IfStateEsixsts(userId))
+                {
+                    command.CommandText = $"UPDATE state_reservetion SET count_people = '{countPeople}' WHERE user_id LIKE '{userId}'";
+                }
+                else
+                {
+                    command.CommandText = $"INSERT INTO state_reservetion (user_id, count_people) values (@user_id, @count_people)"
+                    command.Parameters.AddWithValue("@user_id", userId);
+                    command.Parameters.AddWithValue("@count_people", countPeople);
+                }
+
+                await command.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+            }
+
+        }
+        public async static Task AddDateState(string userId, string date)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"UPDATE state_reservetion SET date = '{date}' WHERE user_id LIKE '{userId}')";
+
+                await command.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+            }
+
+        }
+
+        public async static Task AddTimeState(string userId, string time)
+        {
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+                command.CommandText = $"UPDATE state_reservetion SET time = '{time}' WHERE user_id LIKE '{userId}')";
+
+                await command.ExecuteNonQueryAsync();
+                await connection.CloseAsync();
+            }
+
+        }
+        public async static Task<string> GetInfoState(string userId, string info)
+        {
+            string infoState = string.Empty;
+
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var command = new SqliteCommand();
+                command.Connection = connection;
+
+                switch (info)
+                {
+                    case "countPeople":
+                        command.CommandText = $"select count_people FROM state_reservetion WHERE user_id LIKE '{userId}'";
+                        break;
+                    case "date":
+                        command.CommandText = $"select date FROM state_reservetion WHERE user_id LIKE '{userId}'";
+                        break;
+                    case "time":
+                        command.CommandText = $"select time FROM state_reservetion WHERE user_id LIKE '{userId}'";
+                        break;
+                    default:
+                        break;
+                }
+
+                var reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    infoState = reader.GetString(0);
+                }
+                return infoState;
+
+                
+            }
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,23 +1,117 @@
-﻿using Telegram.Bot;
+﻿using System.Net;
+using Telegram.Bot;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace restaurantBot
 {
     internal class Program
     {
+        enum StateReserve
+        {
+            ChoiceCountPeople,
+            ChoiceDateTime,
+            ShowTables
+
+        }
+        private StateReserve _stateReserve;
+
         static void Main(string[] args)
         {
             var bot = new TelegramBotClient("6717902573:AAFllwaelabWcpQyJI6_BjO8PUOQ1aNWhT4");
 
             bot.StartReceiving(Update,Error);
+
+            Console.ReadLine();
         }
 
         private static async Task Update(ITelegramBotClient bot, Update update, CancellationToken cts)
         {
+            if (update != null)
+            {
+                if (update.Type == UpdateType.Message && update?.Message?.Text != null)
+                {
+                    await HandleMessage(bot,update.Message);
+                }
+                else if (update.Type == UpdateType.CallbackQuery)
+                {
+                    await HandleCallbackQueary(bot,update.CallbackQuery);
+                }
+
+            }
+
+        }
+        private static async Task HandleMessage(ITelegramBotClient bot, Message message)
+        {
+            if (message.Text != null)
+            {
+                var msg = message.Text;
+
+                if (msg == "/start") 
+                {
+
+                    var userName = message.Chat.FirstName;
+
+                    if (userName != null)
+                    {
+                        userName = "no username";
+                    }
+
+                    await DataBase.AddUser(message.Chat.Id.ToString(),DateTime.UtcNow.ToString(),userName);
+                    
+                    await bot.SendTextMessageAsync(message.Chat.Id,"Приветствую! \n Вы можете забронировать столик в наш ресторан прямо сейчас. " +
+                        "\n Или же выберите что вас интересует", replyMarkup: ShowInlineReserveButton());
+                     
+                }
+                else 
+                {
+                    await bot.SendTextMessageAsync(message.Chat.Id, "Вы");
+                }
+
+
+            }
 
 
         }
+
+        private static async Task HandleCallbackQueary(ITelegramBotClient bot, CallbackQuery? callback)
+        {
+            if (callback.Data == "bron")
+            {
+                await bot.SendTextMessageAsync(callback.Message.Chat.Id, "Выберите на какое количество людей вы хотите забронировать столик ", replyMarkup: ShowInlineCountPeopleButton());
+
+            }
+            else if (callback.Data.Contains('-'))
+            { 
+                List<string> days = GetDaysInMonth();
+                await bot.SendTextMessageAsync(callback.Message.Chat.Id,"Выберите дату бронирования",replyMarkup: ShowInlineDateTimeReservation(days, "days"));
+
+                await DataBase.AddCountPeopleState(callback.Message.Chat.Id.ToString(),callback.Data);
+            }
+
+            else if (callback.Data.Contains("days"))
+            { 
+                List<string> hours = GetTimeDay();
+                await bot.SendTextMessageAsync(callback.Message.Chat.Id, "Выберите время для бронирования", replyMarkup: ShowInlineDateTimeReservation(hours, "time"));
+                
+                string dateState = callback.Data.Substring(4);
+                await DataBase.AddDateState(callback.Message.Chat.Id.ToString(),dateState);
+            }
+
+            else if (callback.Data.Contains("time"))
+            { 
+                string timeState = callback.Data.Substring(4);
+                await DataBase.AddTimeState(callback.Message.Chat.Id.ToString(), timeState);
+
+
+            }
+                    
+
+        }
+
         private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
         {
             var ErrorMessage = exception switch
@@ -28,6 +122,101 @@ namespace restaurantBot
             };
             Console.WriteLine(ErrorMessage);
             return Task.CompletedTask;
+        }
+
+        private static InlineKeyboardMarkup ShowInlineReserveButton()
+        {
+            List<InlineKeyboardButton[]> buttonRows = new List<InlineKeyboardButton[]>();
+
+            buttonRows.Add(new[]
+            {
+                InlineKeyboardButton.WithCallbackData(text: "Забронировать","bron")
+            });
+            return new InlineKeyboardMarkup (buttonRows);
+        }
+
+        private static InlineKeyboardMarkup ShowInlineCountPeopleButton()
+        {
+            List<InlineKeyboardButton[]> buttonRows = new List<InlineKeyboardButton[]>();
+
+            buttonRows.Add(new[]
+            {
+                InlineKeyboardButton.WithCallbackData(text: "1 - 2", "1-2"),
+                InlineKeyboardButton.WithCallbackData(text: "3 - 4", "3-4"),
+                InlineKeyboardButton.WithCallbackData(text: "5 - 6", "5-6")
+            });
+            return new InlineKeyboardMarkup(buttonRows);
+        }
+
+        private static InlineKeyboardMarkup ShowInlineDateTimeReservation(List<string> dateTime, string infoDateTime)
+        { 
+            var inlineButtons = new List<List<InlineKeyboardButton>>();
+            List<string> daysInMonth = dateTime;
+
+            var groups = daysInMonth.Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / 4)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+
+            foreach (var group in groups)
+            {
+                var buttonsRow = new List<InlineKeyboardButton>();
+
+                foreach (var day in group)
+                {
+                    buttonsRow.Add(InlineKeyboardButton.WithCallbackData(day.ToString(), $"{infoDateTime}" + day.ToString()));
+                }
+
+                inlineButtons.Add(buttonsRow);
+            }
+            return new InlineKeyboardMarkup(inlineButtons);
+
+        }
+        private static InlineKeyboardMarkup ShowInlineTableReservation(List<string> tables)
+        {
+            List<InlineKeyboardButton[]> buttonRows = new List<InlineKeyboardButton[]>();
+
+            foreach (var table in tables)
+            {
+                buttonRows.Add(new[]
+                {
+                    InlineKeyboardButton.WithCallbackData(text: $"Столик N {table}", $"table {table}" )
+                });
+                    
+                    
+            }
+            return new InlineKeyboardMarkup(buttonRows);
+        }
+
+        private static List<string> GetTimeDay()
+        {
+            List<string> hours = new List<string>();
+            DateTime time = DateTime.UtcNow.AddHours(3);
+            DateTime startDay = DateTime.Now.Date;
+
+            DateTime finallyHour = startDay.AddDays(1).AddSeconds(-1);
+
+            for (DateTime currentHour = time; currentHour <= finallyHour; 
+                currentHour = currentHour.AddHours(1))
+            {
+                hours.Add(new DateTime(currentHour.Year,currentHour.Month,currentHour.Day,  currentHour.Hour, 0, 0).ToString("t"));
+            }
+            return hours;
+        }
+        private static List<string> GetDaysInMonth()
+        {
+            List<string> days = new List<string>();
+            DateTime date = DateTime.UtcNow;
+            int lastDayMonth = DateTime.DaysInMonth(date.Year, date.Month);
+            int countDays = 0;
+
+                for (int i = date.Day; i <= lastDayMonth; i++)
+                {
+                    days.Add(date.AddDays(countDays).ToString("M"));
+                    countDays++;
+                }
+                return days;
+
         }
 
     }
